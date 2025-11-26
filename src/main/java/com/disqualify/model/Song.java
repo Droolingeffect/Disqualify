e6bebabff9cbcea8d55978481d5321c148445ade
aaanom <monakosmos@gmail.com>
2025-11-26 19:16:53 +0400

    Added song.java for search backend

---
+++ src/main/java/com/disqualify/model/Song.java
@@ -0,0 +1,35 @@
+package com.disqualify;
+
+public class Song {
+
+    private int id;
+    private String title;
+    private String album;
+    private String artist;
+    private String genre;
+    private int year;
+    private String coverImage;
+
+    public Song() {}
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAlbum() { return album; }
+    public void setAlbum(String album) { this.album = album; }
+
+    public String getArtist() { return artist; }
+    public void setArtist(String artist) { this.artist = artist; }
+
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
+
+    public String getCoverImage() { return coverImage; }
+    public void setCoverImage(String coverImage) { this.coverImage = coverImage; }
+}
