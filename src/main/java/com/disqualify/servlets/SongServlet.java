80482f7341be6927130b7cb2c96aa93a50710538
b00095564-eng <b00095564@aus.edu>
2025-11-27 16:53:49 +0400

    Create SongServlet.java
    reated songservlet

---
+++ src/main/java/com/disqualify/servlets/SongServlet.java
@@ -0,0 +1,53 @@
+package com.disqualify.servlets;
+
+import com.disqualify.model.Song;
+import com.disqualify.util.DatabaseUtil; 
+import com.google.gson.Gson;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/api/songs")
+public class SongServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        List<Song> songs = new ArrayList<>();
+        
+        try (Connection conn = DatabaseUtil.getConnection()) {
+            String sql = "SELECT * FROM songs ORDER BY created_at DESC LIMIT 10";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                Song song = new Song();
+                song.setId(rs.getInt("id"));
+                song.setTitle(rs.getString("title"));
+                song.setArtist(rs.getString("artist"));
+                song.setAlbum(rs.getString("album"));
+                song.setGenre(rs.getString("genre"));
+                song.setYear(rs.getInt("year"));
+                song.setCoverImage(rs.getString("cover_image"));
+                song.setDuration(rs.getInt("duration"));
+                song.setFilePath(rs.getString("file_path"));
+                song.setReleaseDate(rs.getDate("release_date"));
+                
+                songs.add(song);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Return empty list on error
+        }
+        
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(new Gson().toJson(songs));
+    }
+}
